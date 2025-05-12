f53abe68c4b33d237cce1e0406db68ac2c62fc12
Soy Nelson <juareznelsonelias@gmail.com>
2025-05-12 11:27:04 -0500

    camnbio de nombre

--- app/src/main/java/com/example/proyectito/MainActivity2.kt
+++ app/src/main/java/com/example/proyectito/MainActivity2.kt
@@ -13,7 +13,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.administrar_aplicaciones)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
